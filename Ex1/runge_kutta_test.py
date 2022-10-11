da1ecff92aaadd2f4590e6a295ffee2a87e5749d
fl3cky <florian.wallner@student.tugraz.at>
2022-10-11 19:25:25 +0200

    Erweiterung mit Energie

--- Ex1/runge_kutta_test.py
+++ Ex1/runge_kutta_test.py
@@ -17,6 +17,12 @@ b_rk4 = np.array([1/6, 1/3, 1/3, 1/6])
 
 c_rk4 = np.array([0, 0.5, 0.5, 1])
 
+a_eul = np.array([[0]])
+
+b_eul = np.array([1])
+
+c_eul = np.array([0])
+
 
 def explicit_runge_kutta(F, y0, t0, t_max, epsilon, a, b, c):
     
@@ -57,15 +63,29 @@ def dfdt(f, t):
     
     return np.array([p, -omega**2*np.sin(theta)])
 
-
+m = 1
+l = 1
+g = 9.81
+omega = np.sqrt(g/l)
 y0 = np.array([np.pi/3, 0])
 t0 = 0
 t_max = 10
-epsilon = 0.1
+epsilon = 0.05
 
-y, t = explicit_runge_kutta(dfdt, y0, t0, t_max, epsilon, a_rk4, b_rk4, c_rk4)
+y_rk4, t_rk4 = explicit_runge_kutta(dfdt, y0, t0, t_max, epsilon, a_rk4, b_rk4, c_rk4)
 
 
-plt.plot(t, y[0,:])
+
+y_eul, t_eul = explicit_runge_kutta(dfdt, y0, t0, t_max, epsilon, a_eul, b_eul, c_eul)
+
+plt.plot(t_eul, y_eul[0,:])
+plt.plot(t_rk4, y_rk4[0,:])
+
+E_t_rk4 = 0.5*m*l**2*y_rk4[1,:]**2 + m*g*l*(1 - np.cos(y_rk4[0,:]))
+
+E_t_eul = 0.5*m*l**2*y_eul[1,:]**2 + m*g*l*(1 - np.cos(y_eul[0,:]))
+
+#plt.plot(t_rk4, E_t_rk4, color = 'skyblue')
+#plt.plot(t_eul, E_t_eul, color = 'crimson')
 
     
